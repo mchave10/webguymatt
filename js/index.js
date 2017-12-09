2a19ff4cf910d80f3fad70f56fca9f4c7d512792
mchave10 <mchave10@hotmail.com>
2017-12-09 11:47:02 -0500

    fix

--- js/index.js
+++ js/index.js
@@ -1,5 +1,25 @@
 $(document).ready(function(){
-    setTimeout(function(){
-    $("html,body").css("overflow-y","visible");
-    }, 2000);    
+    function shakeTaxi() {
+        setTimeout(function(){
+            $(".taxi").addClass("shake bounce");   
+        }, 1000);     
+    }
+    function slideOutTaxi() {
+        setTimeout(function(){
+            $(".taxi").addClass("slideOutRight");  
+         }, 2200);     
+    }
+    $(".mattbouncearrow").click(function(){
+        setTimeout(function(){
+            $("html,body").css("overflow-y","visible");
+        }, 100);  
+        var nextSection = $(this).parents().next('section')
+        nextSection.fadeIn();
+        $("html,body").delay(1000).animate({
+            scrollTop: nextSection.offset().top
+        }, 500, 'swing');
+        shakeTaxi();                                               
+        slideOutTaxi();                          
+        slideIn
+    });
 });

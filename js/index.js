53d86d5baf807453eeaf6b90e65e0ef6d4cf5cee
mchave10 <mchave10@hotmail.com>
2017-12-12 23:23:19 -0500

    taxi overflow issue

--- js/index.js
+++ js/index.js
@@ -27,7 +27,9 @@ $(document).ready(function(){
         setTimeout(function(){
             $(".taxi").addClass("slideOutRight");  
          }, 2200);    
+            
         setTimeout(function(){
+            $(".taxi").css("display","none"); 
             $(".aboutContent").fadeIn();
             
          }, 2200);    
